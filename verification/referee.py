2f48d78a58cf2bc270d96d799294640e0dc4be80
Oleksandr Zozulia <freemanlex@gmail.com>
2022-08-11 20:32:45 +0300

    next-api hints uk uk-hints

--- verification/referee.py
+++ verification/referee.py
@@ -28,8 +28,7 @@ checkio.referee.cover_codes
 
 from checkio.signals import ON_CONNECT
 from checkio import api
-from checkio.referees.io import CheckiOReferee
-from checkio.referees import cover_codes
+from checkio.referees.io_template import CheckiOReferee
 
 from tests import TESTS
 
@@ -38,11 +37,13 @@ api.add_listener(
     CheckiOReferee(
         tests=TESTS,
         function_name={
-            "python": "nearest_square",
-            "js": "nearestSquare"
+            "python": "nearest_square", # check function name!
+            "js": "nearestSquare"      # check function name!
         },
         cover_code={
-            #'python-3': cover_codes.unwrap_args,
-            #'js-node': cover_codes.js_unwrap_args
+            'python-3': {},
+            'js-node': {
+                # "dateForZeros": True,
+            }
         }
     ).on_ready)
